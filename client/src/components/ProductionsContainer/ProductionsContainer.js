6f1d7832b9bb59da3d8717f4c406f5dab70b7d60
Tom Tobar <cocdetombomb@gmail.com>
2023-09-17 22:22:08 -0500

    add home and productions container

---
+++ client/src/components/ProductionsContainer/ProductionsContainer.js
@@ -0,0 +1,10 @@
+import './styles.css'
+
+const ProductionsContainer = ({productions}) => {
+  return (
+    <div className='productions-container'>
+        <h1 className='productions-heading'><span className='heading-highlight'>F</span>latiron Theater <span className='heading-highlight'>C</span>ompany</h1>
+    </div>
+  )
+}
+export default ProductionsContainer
